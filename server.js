e903395dd9cc2ae3cf8ba529d1018bc497258d23
TiffanieJ <tjohns29@gmu.edu>
2016-03-06 16:00:23 -0500

    initialize repo

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+//(req,res,next) --order matters
+// logger logs out GET request
+var middleware = {
+	requireAuthentication: function (req,res,next) {
+		//console.log('private route hit!');
+		next();
+	},
+	logger: function (req,res,next) {
+		console.log('Request: '+req.method + req.originalUrl + new Date().toString());
+		next();
+
+	}
+};
+//order is importsant put middleware up top
+//app.use(middleware.requireAuthentication);
+
+//req holds all in from user like url, cookies, json
+//res is what we send back
+
+app.use(middleware.logger);
+app.get('/about', middleware.requireAuthentication, function (req,res) {
+	res.send('About us!')
+});
+
+app.use(express.static(__dirname + '/public'));
+//console.log(__dirname);
+
+app.listen(PORT, function() {
+	console.log('Express server started on server ' + PORT);
+});
